c7439998fdde1ba5d78c1cdb7432345a0085f79a
maxgoover <maxgoover@gmail.com>
2020-08-14 02:10:55 +0300

    Finished course guzzle

--- public/index.php
+++ public/index.php
@@ -3,4 +3,26 @@
 declare(strict_types=1);
 
 require_once __DIR__ . '/../bootstrap.php';
-require_once __DIR__ . '/../src/router/router.php';
+//require_once __DIR__ . '/../src/router/router.php';
+
+use GuzzleHttp\Client;
+
+$client = new Client([
+    'base_uri' => 'http://httpbin.org',
+    'timeout' => 2.0
+]);
+
+$response = $client->request('GET', 'ip');
+
+echo $response->getStatusCode();
+$body = $response->getBody();
+
+echo $body->getContents();
+
+echo '<pre>';
+print_r(get_class_methods($body));
+echo '</pre>';
+
+echo '<pre>';
+print_r(get_class_methods($response));
+echo '</pre>';
